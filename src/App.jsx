a95a2932d147ee3c7918015ad3a0a92d23732543
Kieran Best <best.kieran.4595@gmail.com>
2024-03-07 22:12:01 +0000

    Almost finished navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import styles from "./App.modules.css";
+import styles from "./App.module.css";
+import { Navbar } from "./components/Navbar/Navbar.jsx";
 
 function App() {
 
   return (
-    <div className={styles.App}>Hello World</div>
+    <div className={styles.App}>
+      <Navbar />
+    </div>
   )
 }
 
