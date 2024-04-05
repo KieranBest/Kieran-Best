729cfcd197e3a3ea9215c5e4bf17a77b26bf1922
Kieran Best <best.kieran.4595@gmail.com>
2024-04-05 21:42:27 +0100

    Fixed Skills

--- src/Components/About/Skills.jsx
+++ src/Components/About/Skills.jsx
@@ -7,20 +7,21 @@ import skills from "../../data/skills.json";
 export const Skills = () => {
     return (
         <section>
-            <div className="flex space-x-16">
-                <div className="flex space-x-16 animate-loop-scroll">
+            <div className="relative flex overflow-x-hidden px-6">
+                <div className="flex py-12 animate-marquee whitespace-nowrap">
                     {skills.map((skill) => (
-                        <div key={skill.id} className="bg-tranparent rounded-full opacity-75 flex items-center justify-center">
-                            <img loading="lazy" src={getImageUrl(skill.image)} alt={skill.name} className="max-w-20"/>
-                        </div>
-                    ))}
+                            <div key={skill.id} className="bg-tranparent rounded-full opacity-75 flex items-center justify-center px-6">
+                                <img src={getImageUrl(skill.image)} alt={skill.name} className="max-w-20"/>
+                            </div>
+                        ))}
                 </div>
-                <div className="flex space-x-16 animate-loop-scroll" aria-hidden="true">
+
+                <div className="flex absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
                     {skills.map((skill) => (
-                        <div key={skill.id} className="bg-transparent rounded-full opacity-75 flex items-center justify-center">
-                            <img loading="lazy" src={getImageUrl(skill.image)} alt={skill.name} className="max-w-20" />
+                        <div key={skill.id} className="bg-tranparent rounded-full opacity-75 flex items-center justify-center px-6">
+                            <img src={getImageUrl(skill.image)} alt={skill.name} className="max-w-20"/>
                         </div>
-                    ))}
+                    ))}  
                 </div>
             </div>
         </section>
