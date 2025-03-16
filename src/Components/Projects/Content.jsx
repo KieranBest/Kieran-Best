92f6e4a70ddb5b9afbc9b068ef34f4aeb6d8a4c1
Kieran Best <best.kieran.4595@gmail.com>
2025-03-16 22:02:18 +0000

    Update portfolio layout
    remove unwanted test images

--- src/Components/Projects/Content.jsx
+++ src/Components/Projects/Content.jsx
@@ -5,20 +5,21 @@ export const Content = () => {
     const imgSrc = "../../../assets/"
     return (
         <section>
-            <div className="mx-auto w-full sm:w-3/4 lg:w-1/2">
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 ">
-                    {portfolio.map((s) => (
-                        <a href={s.source} target="_blank" key={s.id}>
-                            <div className="bg-black m-10 p-10 w-full">
-                                <img alt={s.title} src={s.image}
-                                    className=""
-                                />
-                                <p className="text-base pb-40">{s.title} - {s.description}</p>
-                            </div>
-                        </a>
-                    ))}
+            <div className="w-5/6 mx-auto">
+                <div className="grid grid-cols-1 lg:grid-cols-2 pb-10 gap-4">
+                        {portfolio.map((s) => (
+                            <a href={s.source} target="_blank" key={s.id} 
+                                className="flex flex-col items-center bg-white border border-gray-200 rounded-lg md:flex-row hover:bg-gray-200 transition duration-300 ease-in-out dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                <img alt={s.title} src={s.image} className="object-cover w-full rounded-t-lg h-96 md:h-full md:w-1/2 md:rounded-none md:rounded-s-lg"/>
+                                <div className="m-auto text-center p-4 leading-normal">
+                                    <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{s.title}</p>
+                                    <p className="mb-3 text-black dark:text-white">{s.description}</p>
+                                </div>
+                            </a>
+                        ))}
+                    </div>
                 </div>
-            </div>
+            
         </section>
     )
 }
