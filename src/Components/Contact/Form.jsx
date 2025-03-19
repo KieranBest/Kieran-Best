ad2604bf0c91810b77225538a682a583c81f8c04
Kieran Best <best.kieran.4595@gmail.com>
2025-03-19 09:07:32 +0000

    Store images on s3

--- src/Components/Contact/Form.jsx
+++ src/Components/Contact/Form.jsx
@@ -123,7 +123,7 @@ export const Form = () => {
                 </form>
                 <div className='items-center justify-center m-8'>
                     <a href="https://www.linkedin.com/in/kieransbest/" target="_blank" >
-                        <img src='src/Assets/Contact/linkedin.png' className="bg-white rounded-lg h-10 w-10" />
+                        <img src='https://s3.eu-west-2.amazonaws.com/www.bestwebdev.co.uk/Images/Contact/linkedin.png' className="bg-white rounded-lg h-10 w-10" />
                     </a>
                 </div>
             </div>
