b647f24bc1c8ee7955131ccaf185cc95c1ab8066
Kieran Best <best.kieran.4595@gmail.com>
2025-03-22 20:00:10 +0000

    update project cards

--- src/Components/Projects/Content.jsx
+++ src/Components/Projects/Content.jsx
@@ -9,7 +9,7 @@ export const Content = () => {
                         {portfolio.map((s) => (
                             <a href={s.source} target="_blank" key={s.id} 
                                 className="flex flex-col items-center bg-white border border-gray-200 rounded-lg md:flex-row hover:bg-gray-200 transition duration-300 ease-in-out dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                                <img alt={s.title} src={s.image} className="object-cover w-full rounded-t-lg h-96 md:h-full md:w-1/2 md:rounded-none md:rounded-s-lg"/>
+                                <img alt={s.title} src={s.image} className="h-48 w-96 object-cover rounded-t-lg rounded-none rounded-s-lg"/>
                                 <div className="m-auto text-center p-4 leading-normal">
                                     <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{s.title}</p>
                                     <p className="mb-3 text-black dark:text-white">{s.description}</p>
